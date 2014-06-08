ca06a4d59a4683d082a702f123e816b2c6718aa4
Sam Malone <samicemalone@gmail.com>
2014-06-08 19:11:36 +0100

    Bumped version to 1.3.0

--- src/main/java/uk/co/samicemalone/tv/Version.java
+++ src/main/java/uk/co/samicemalone/tv/Version.java
@@ -34,7 +34,7 @@ package uk.co.samicemalone.tv;
  */
 public class Version {
     
-    public static final String VERSION = "1.2.2";
-    public static final String BUILD_DATE = "February 28 2014";
+    public static final String VERSION = "1.3.0";
+    public static final String BUILD_DATE = "June 08 2014";
     
 }
